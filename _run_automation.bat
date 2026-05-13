98fd4d169f1f3045a0452f1f16c9c586ae82d694
apmhAI <apmhiai85@gmail.com>
2026-05-13 12:32:03 +0530

    Delete _run_automation.bat

--- _run_automation.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "C:\Users\DELL_001\Downloads\Cauelist UI"
-"C:\Users\DELL_001\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\DELL_001\Downloads\Cauelist UI\court_automation.py" --date 15-04-2026 --nclat --no-prompt
-echo.
-echo === Automation finished. You may close this window. ===
-pause
